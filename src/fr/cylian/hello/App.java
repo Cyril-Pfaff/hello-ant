3f341ffc08f7c4582adfb42f59f291bdb6fb7170
Cyril Pfaff <cp512@cylian.org>
2021-12-06 15:38:30 +0100

    Update v3

--- src/fr/cylian/hello/App.java
+++ src/fr/cylian/hello/App.java
@@ -3,7 +3,7 @@ package fr.cylian.hello;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println(" Hello World !!! - v2");
+		System.out.println(" Hello World !!! - v3");
 	}
 
 }
