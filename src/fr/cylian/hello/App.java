8f301b28f93e10e721bf8c87c6f2a235254f348c
Cyril Pfaff <cp512@cylian.org>
2021-12-06 15:37:40 +0100

    Update v2

--- src/fr/cylian/hello/App.java
+++ src/fr/cylian/hello/App.java
@@ -3,8 +3,7 @@ package fr.cylian.hello;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(" Hello World !!! ");
+		System.out.println(" Hello World !!! - v2");
 	}
 
 }
